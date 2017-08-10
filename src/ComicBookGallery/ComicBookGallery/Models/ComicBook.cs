c0a81b3ccbe1ae3f2c6b2bd94065f3ec2cbbf8f2
Devarsh14 <Devarsh14@gmail.com>
2017-08-11 04:57:21 +1000

    Addeed comicbook and artist Models and created Array of artist class first ime

---
+++ src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+
+        // Important for MVc
+
+            // First step towards creating Data-modelling 
+            // Process of structure data first=== Data model Nounce is noun or class name or data model 
+            // here first noun is comic book we can define fields or properties 
+
+        public int ID { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+
+
+        public bool Favorite { get; set; }
+
+
+
+
+
+
+    }
+}
